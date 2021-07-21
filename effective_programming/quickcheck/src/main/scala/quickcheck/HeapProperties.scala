d2ad7e89319ddecca56b5f796d6c443fb52d28a8
Kai <kaiyhirota@gmail.com>
2021-07-22 00:35:02 +1000

    finished week 4

--- effective_programming/quickcheck/src/main/scala/quickcheck/HeapProperties.scala
+++ effective_programming/quickcheck/src/main/scala/quickcheck/HeapProperties.scala
@@ -22,19 +22,18 @@ class HeapProperties(heapInterface: HeapInterface) extends Properties("Heap"):
   property("the minimum of a heap of two elements should be the smallest of the two elements") =
     forAll { (x1: Int, x2: Int) =>
       val heap = insert(x2, insert(x1, empty))
-      val min: Int = ???
+      val min: Int = Math.min(x1, x2)
       findMin(heap) == min
     }
 
   property("delete minumum of heap of one element should return an empty heap") =
     forAll { (x: Int) =>
       // create a heap with exactly one element, `x`
-      val heap1: List[Node] = ???
+      val heap1: List[Node] = insert(x, empty)
       // delete the minimal element from it
-      val heap0: List[Node] = ???
+      val heap0: List[Node] = deleteMin(heap1)
       // check that heap0 is empty
-      ???
-      
+      isEmpty(heap0)
     }
 
   property("continually finding and deleting the minimal element of a heap should return a sorted sequence") =
@@ -46,23 +45,54 @@ class HeapProperties(heapInterface: HeapInterface) extends Properties("Heap"):
         true
       else
         // find the minimal element
-        val x1: Int = ???
+        val x1: Int = findMin(heap)
         // delete the minimal element of `heap`
-        val heap2: List[Node] = ???
+        val heap2: List[Node] = deleteMin(heap)
         // find the minimal element in `heap2`
-        val x2: Int = ???
+        val x2: Int = findMin(heap2)
         // check that the deleted element is smaller than the minimal element
         // of the remaining heap, and that the remaining heap verifies the
         // same property (by recursively calling `check`)
-        val checked: Boolean = ???
+        val checked: Boolean = x1 <= x2
         checked
     // check arbitrary heaps
     forAll { (heap: List[Node]) =>
       check(heap)
     }
 
-  // TODO Write more properties here to detect the bugs
-  // in bogus BinomialHeap implementations
+  // for any heap, adding the minimal element, and then finding it, should return the element in question
+  property("gen1") = forAll { (heap: List[Node]) =>
+    val m = if isEmpty(heap) then 0 else findMin(heap)
+    findMin(insert(m, heap)) == m
+  }
+
+  // adding a single element to an empty heap, and then removing this element, should yield the element in question
+  property("min1") = forAll { (a: Int, heap: List[Node]) =>
+    val heap = insert(a, empty)
+    findMin(heap) == a
+  }
+
+  // If you insert any two elements into an empty heap, finding the minimum of the resulting heap should get the smallest of the two elements back.
+  property("insert 2 distinct values and get minimum should return the lower value") = forAll { (x: Int, y: Int) =>
+    val heap = insert(x, insert(y, empty))
+    if x >= y then findMin(heap) == y else findMin(heap) == x
+  }
+
+  // If you insert an element into an empty heap, then delete the minimum, the resulting heap should be empty.
+  property("insert one element into an empty map, delete the min, should result in empty heap") =
+    forAll { (x: Int) =>
+      deleteMin(insert(x, empty)) == empty
+    }
+
+
+  // Given any heap, you should get a sorted sequence of elements when continually finding and deleting minima. (Hint: recursion and helper functions are your friends.)
+  property("finding and deleting minimum should yield sorted sequence of elements") = forAll {
+    (x: Int, y: Int, z: Int) =>
+      def popMin(heap: List[Node]): List[Int] =
+        if !isEmpty(heap) then findMin(heap) :: popMin(deleteMin(heap))
+        else Nil
+      popMin(insert(x, insert(y, insert(z, empty)))) == List(x, y, z).sorted
+  }
   
 
   // random heap generator --- DO NOT MODIFY
